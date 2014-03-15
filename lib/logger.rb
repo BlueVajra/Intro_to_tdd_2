28e5b8ffe03942ba1d6547f28b074d3426b71151
BlueVajra <cory.leistikow@gmail.com>
2014-03-15 12:27:42 -0600

    returns all logs after method has been called 5 times

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,5 +1,15 @@
 class Logger
   def initialize(filename)
     @filename = filename
+    @log_storage = []
+    @count = 0
+  end
+  def log(message)
+    @count += 1
+    @message = message
+    @log_storage << @message
+    if @count == 5
+      @log_storage.join("\n")
+    end
   end
 end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -2,9 +2,35 @@ require 'logger'
 
 describe Logger do
   it "initializes with a filename" do
-
     filename = "Logger.txt"
     logger = Logger.new(filename)
+  end
+  it "receives a string with the log method" do
+    filename = "Logger.txt"
+    logger = Logger.new(filename)
+
+    logger.log("This is our first string")
+
+  end
+  it "prints out the message after 5 logs have been added" do
+    filename = "Logger.txt"
+    logger = Logger.new(filename)
+
+    logger.log("This is our first string")
+    logger.log("This is our second string")
+    logger.log("This is our third string")
+    logger.log("This is our fourth string")
+
+
+    expected = "This is our first string
+This is our second string
+This is our third string
+This is our fourth string
+This is our fifth string"
+
+    actual = logger.log("This is our fifth string")
+
+    expect(actual).to eq expected
 
   end
 
