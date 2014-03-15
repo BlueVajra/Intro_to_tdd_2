e503c15b163edb9c27edf132fc41fa0bbbe8941f
BlueVajra <cory.leistikow@gmail.com>
2014-03-15 16:18:22 -0600

    handles more then 5 logs, but still prints 5 at a time

--- lib/logger.rb
+++ lib/logger.rb
@@ -3,13 +3,13 @@ class Logger
     @filename = filename
     @log_storage = []
     @count = 0
-    @file = File.new(@filename, "w+")
   end
   def log(message)
     @count += 1
     @message = message
     @log_storage << Time.now.to_s + " " + @message
     if @count == 5
+      @file = File.new(@filename, "w+")
       @file.write(@log_storage.join("\n"))
       @file.close
       @count =0
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -43,5 +43,20 @@ describe Logger do
     expect(actual).to match("fourth string is also here")
 
   end
+  it "only prints 10 lines if 13 have been logged" do
+    filename = "Logger.txt"
+    logger = Logger.new(filename)
+    count = 1
+    13.times do
+      logger.log("This is string ##{count}")
+      count += 1
+    end
+
+    actual = File.read(File.expand_path("../../Logger.txt", __FILE__))
+
+    expect(actual).to match("This is string #9")
+    expect(actual).not_to match("This is string #12")
+
+  end
 
 end
